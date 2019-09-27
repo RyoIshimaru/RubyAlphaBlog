cd80f19aedef6d35727b17374434386c24c1e60e
RyoIshimaru <ryo.ishimaru.kyoto@gmail.com>
2019-09-27 05:02:29 -0700

    Added edit function of user account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_param)
+    @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to Ryo Blog #{@user.username}!"
       redirect_to articles_path
@@ -13,8 +13,22 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Your account was suucessfully updated."
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+  
   private
-  def user_param
+  def user_params
     params.require(:user).permit(:username, :email, :password)
   end
 end
